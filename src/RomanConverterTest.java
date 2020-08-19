46b8b6fb3aeaf2ccdfe26a7c726211468229d0fc
cecimiv <ceciversen@gmail.com>
2020-08-19 19:40:37 +0200

    knl

--- src/RomanConverterTest.java
+++ src/RomanConverterTest.java
@@ -4,12 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanConverterTest {
 
-    private String toRoman(int num){
-        return null;
+    private String toRoman(int i){
+        String result = "I";
+        if(i==2){
+            result =  "II";
+        }else if(i==3){
+            result =  "III";
+        }
+        return result;
         }
 
     @Test
     void shouldConvert1ToI(){
         assertEquals("I", toRoman(1));
+    }
+    @Test
+    void shouldConvert2ToII(){
+        assertEquals("II", toRoman(2));
+    }
+
+    @Test
+    void shouldConvert3ToI(){
+        assertEquals("III", toRoman(3));
     }
 }
